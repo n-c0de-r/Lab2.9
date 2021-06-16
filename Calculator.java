1c8cfe5d51bfa154867478743a92dd5eb04224f1
N1co c0des <78597469+n-c0de-r@users.noreply.github.com>
2021-06-16 21:04:50 +0200

    Clean up code and comments

--- Calculator.java
+++ Calculator.java
@@ -1,38 +1,33 @@
 /**
- * The main class of a simple calculator. Create one of these and you'll
- * get the calculator on screen.
+ * The main class of a simple calculator. Create one of
+ * these and you'll get the calculator on screen.
  * 
  * @author David J. Barnes and Michael Kolling
  * @version 2008.03.30
  * 
- * @author Alex and n-c0de-r
+ * @author Alexjger and n-c0de-r
  * @version 27.05.2021
  */
-public class Calculator
-{
-    private CalcEngineString engine;
-    private UserInterfaceString gui;
+public class Calculator {
+	private CalcEngineString engine;
+	private UserInterfaceString gui;
 
-    /**
-     * Create a new calculator and show it.
-     */
-    public Calculator()
-    {
-        //engine = new CalcEngine();
-        //gui = new UserInterface(engine);
-    	engine = new CalcEngineString();
-        gui = new UserInterfaceString(engine);
-    }
+	/**
+	 * Create a new calculator and show it.
+	 */
+	public Calculator() {
+		engine = new CalcEngineString();
+		gui = new UserInterfaceString(engine);
+	}
 
-    /**
-     * In case the window was closed, show it again.
-     */
-    public void show()
-    {
-        gui.setVisible(true);
-    }
-    
-    public static void main(String[] args) {
-    	Calculator c = new Calculator();
-    }
+	/**
+	 * In case the window was closed, show it again.
+	 */
+	public void show() {
+		gui.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+	}
 }
